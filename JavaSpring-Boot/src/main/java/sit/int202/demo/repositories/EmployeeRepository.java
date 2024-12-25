a43bfd764ab2985e149425276c182a6aa6561539
EstherChanidapa <142302324+Chanidapaka@users.noreply.github.com>
2024-12-25 20:24:20 +0700

    Delete JavaSpring-Boot directory

--- JavaSpring-Boot/src/main/java/sit/int202/demo/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package sit.int202.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int202.demo.entities.Employee;
-
-public interface giEmployeeRepository extends JpaRepository<Employee, Integer> {
-}
